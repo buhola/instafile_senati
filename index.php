c14fe9bce074d394c740456a58b121c7fbc0181e
vallejo23 <150494637+vallejo23@users.noreply.github.com>
2024-05-14 00:32:55 -0500

    hacerlo 100% responsive
    use @media para hacer responsive el proyecto

--- index.php
+++ index.php
@@ -12,13 +12,15 @@ try {
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_FILES['archivo'])) {
-            $archivo = $_FILES['archivo'];
+            $archivos = $_FILES['archivo'];
 
-            if (move_uploaded_file($archivo['tmp_name'], $carpetaRuta . '/' . $archivo['name'])) {
-                $subido = true;
-                $mensaje = "Archivo subido con éxito.";
-            } else {
-                throw new Exception("Error al subir el archivo.");
+            foreach ($archivos['name'] as $key => $name) {
+                if (move_uploaded_file($archivos['tmp_name'][$key], $carpetaRuta . '/' . str_replace(' ', '_', $name))) {
+                    $subido = true;
+                    $mensaje = "Archivos subidos con éxito.";
+                } else {
+                    throw new Exception("Error al subir el archivo $name.");
+                }
             }
         }
     }
@@ -49,28 +51,24 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Compartir archivos</title>
-    <script src="parametro.js"></script>
     <link rel="stylesheet" href="estilo.css">
 </head>
 
 <body>
     <h1>Compartir archivos <sup class="beta">BETA</sup></h1>
     <div class="content">
-        <h3>Sube tus archivos y comparte este enlace temporal: <span>ibu.pe/?nombre=<?php echo $carpetaNombre;?></span></h3>
+        <h3>Sube tus archivos y comparte este enlace temporal: <span>ibu.pe/<?php echo $carpetaNombre; ?></span></h3>
         <div class="container">
             <div class="drop-area" id="drop-area">
                 <form action="" id="form" method="POST" enctype="multipart/form-data">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" style="fill:#0730c5;transform: ;msFilter:;"><path d="M13 19v-4h3l-4-5-4 5h3v4z"></path><path d="M7 19h2v-2H7c-1.654 0-3-1.346-3-3 0-1.404 1.199-2.756 2.673-3.015l.581-.102.192-.558C8.149 8.274 9.895 7 12 7c2.757 0 5 2.243 5 5v1h1c1.103 0 2 .897 2 2s-.897 2-2 2h-3v2h3c2.206 0 4-1.794 4-4a4.01 4.01 0 0 0-3.056-3.888C18.507 7.67 15.56 5 12 5 9.244 5 6.85 6.611 5.757 9.15 3.609 9.792 2 11.82 2 14c0 2.757 2.243 5 5 5z"></path></svg> <br>
-                    <input type="file" class="file-input" name="archivo" id="archivo" onchange="document.getElementById('form').submit()">
-                    <label> Arrastra tus archivos aquí<br>o</label>
-                    <p><b>Abre el explorador</b></p> 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" style="fill:#0730c5;transform: ;msFilter:;"><path d="M13 19v-4h3l-4-5-4 5h3v4z"></path><path d="M7 19h2v-2H7c-1.654 0-3-1.346-3-3 0-1.404 1.199-2.756 2.673-3.015l.581-.102.192-.558C8.149 8.274 9.895 7 12 7c2.757 0 5 2.243 5 5v1h1c1.103 0 2 .897 2 2s-.897 2-2 2h-3v2h3c2.206 0 4-1.794 4-4a4.01 4.01 0 0 0-3.056-3.888C18.507 7.67 15.56 5 12 5 9.244 5 6.85 6.611 5.757 9.15 3.609 9.792 2 11.82 2 14c0 2.757 2.243 5 5 5z"></path></svg> <br>
+                    <input type="file" class="file-input" name="archivo[]" id="archivo" multiple onchange="document.getElementById('form').submit()">
+                    <label style="font-size: 24px;">Arrastra tus archivos aquí</label>
                     
                 </form>
             </div>
 
             <div class="container2">
-               
-
                 <div id="file-list" class="pila">
                     <?php
                     $targetDir = $carpetaRuta;
@@ -83,20 +81,12 @@ try {
 
                         foreach ($files as $file) {
                             echo "<div class='archivos_subidos'>
+                            <div><svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-folder' width='24' height='24' viewBox='0 0 24 24' stroke-width='2' stroke='currentColor' fill='none' stroke-linecap='round' stroke-linejoin='round'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M3 4v16a2 2 0 0 0 2 2h14a2 2 0 0 0 2 -2v-10l-6 -4l-6 4z' /></svg></div>
                             <div><a href='$carpetaRuta/$file' download class='boton-descargar'>$file</a></div>
                             <div>
                             <form action='' method='POST' style='display:inline;'>
                                 <input type='hidden' name='eliminarArchivo' value='$file'>
-                                <button type='submit' class='btn_delete'>
-                                    <svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-trash' width='24' height='24' viewBox='0 0 24 24' stroke-width='2' stroke='currentColor' fill='none' stroke-linecap='round' stroke-linejoin='round'>
-                                        <path stroke='none' d='M0 0h24v24H0z' fill='none'/>
-                                        <path d='M4 7l16 0' />
-                                        <path d='M10 11l0 6' />
-                                        <path d='M14 11l0 6' />
-                                        <path d='M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12' />
-                                        <path d='M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3' />
-                                    </svg>
-                                </button>
+                                <button type='submit' class='btn_delete'>Eliminar</button>
                             </form>
                         </div>
                         </div>";
@@ -106,6 +96,8 @@ try {
                     }
                     ?>
                 </div>
+                <!-- Botón "Agregar" en la esquina inferior derecha -->
+                <button class="btn-abrir-directorio" onclick="document.getElementById('archivo').click()">Agregar</button>
             </div>
         </div>
     </div>
